caf5347d6ea4dd99ef40c01ae9809357ef0341f0
unknown <vtrolinux@gmail.com>
2021-11-12 23:39:26 -0300

    auth and user routes

---
+++ backend/helpers/generate_keys.js
@@ -0,0 +1,4 @@
+const crypto = require('crypto')
+const base64url = require('base64url')
+const output = base64url(crypto.randomBytes(256).toString('hex'))
+console.log(output)
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+//modules
+const express = require('express')
+const Bodyparser = require('body-parser') 
+const cors = require('cors') 
+const mongoose = require('mongoose')
+//const credentials = require('./credentials.json')
+//routers import
+const authRouter = require('./routers/authRoutes')
+const userRouter =  require('./routers/userRoutes')
+//middleware
+
+
+//config
+
+const port = 3000
+const nomeBanco = 'bancoFight'
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+// routers api's
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+//atrelar rotas ao express
+
+app.get('/',(req,res)=>{
+    console.log(process.env.SECRET)
+    res.json({menssage: 'ok'})
+})
+
+app.listen(port,()=>{
+    console.log(`SERVIDOR SENDO EXECUTADO NA PORTA ${port}`)
+})
+
+//conexao mongodb
+mongoose.connect(`mongodb://localhost/${nomeBanco}`, 
+{
+  useNewUrlParser: true,
+ // useFindAndModify: false,
+  useUnifiedTopology: true
+}) 
